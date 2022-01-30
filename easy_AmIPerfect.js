c4a75d8bd31a21953929deb918dac5d1bc84fe39
Sadashiv <65489912+prompt-07@users.noreply.github.com>
2022-01-30 09:45:57 +0530

    Create easy_AmIPerfect.js

---
+++ easy_AmIPerfect.js
@@ -0,0 +1,24 @@
+
+console.log(perfectCheck(6))
+console.log(perfectCheck(12))
+console.log(perfectCheck(8))
+
+function getFactors(num){
+  let a = []
+  let i=1
+  while(i<=num/2){
+    if(num%i===0)
+      a.push(i)
+      
+    i++
+  }
+  
+  return a;
+}
+
+function perfectCheck(num){
+  let factorsOfNum = getFactors(num)
+  let sum = factorsOfNum.reduce((partialSum,el) => partialSum+el,0);
+  
+  return sum===num ? "Perfect" : sum>num ? "Abundant"  : "Deficient";
+}
